423a07d04045f2f0587e7203e105c326d9c7fb68
Mateo Juvera <mjm973@nyu.edu>
2018-03-01 16:14:19 +0400

    added basic seed shooting

--- Assets/Scripts/SeedShooter.cs
+++ Assets/Scripts/SeedShooter.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class SeedShooter : MonoBehaviour {
 
+    public GameObject seed;
+
+    VRTK_ControllerEvents controller;
+
 	// Use this for initialization
 	void Start () {
-		
+        controller = GetComponent<VRTK_ControllerEvents>();
+        controller.TouchpadReleased += new ControllerInteractionEventHandler(OnTouchpadReleased);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+   void OnTouchpadReleased(object sender, ControllerInteractionEventArgs e) {
+        Debug.Log("yay");
+
+        GameObject s = Instantiate(seed, transform.position, Quaternion.identity);
+        Rigidbody rb = s.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 10f, ForceMode.Impulse);
+    }
 }
